eb2410407e4b9b42e85cfc3e4a0fe40620e91b61
Jhonathan <Jhonathan.Engineer@gmail.com>
2024-11-15 17:07:38 -0300

    chore: add function comprar

---
+++ js/app.js
@@ -0,0 +1,11 @@
+let qtdDisponivelPista = document.getElementById('qtd-pista').textContent;
+let qtdDisponivelSuperior = document.getElementById('qtd-superior').textContent;
+let qtdDisponivelInferior = document.getElementById('qtd-inferior').textContent;
+
+
+function comprar() {
+  let tipoIngresso = document.getElementById('tipo-ingresso').value;
+  let qtdAssento = document.getElementById('qtd').value;
+  
+
+}
